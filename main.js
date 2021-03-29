b79c8a79221ab0d6188cf31f430bd13763f3d1ee
ViachaslauBY <70838254+ViachaslauBY@users.noreply.github.com>
2021-03-30 00:38:42 +0300

    Add files via upload
    add js

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 let cards = document.querySelectorAll('.card');
+let cardsTextSubtitle = document.querySelectorAll('.card__text-subtitle');
+let button = document.querySelector('.button');
+let arr = [
+    'be very happy', 
+    'be famous', 
+    'fall in love', 
+    'be lucky', 
+    'get married',
+    'get a new job',   
+    'get a lot of money',   
+    'get sick',   
+    'be unhappy',   
+    'get a divorce',   
+    'move house',   
+    'travel',   
+]
+
+
+
+for (let i = 0; i < cardsTextSubtitle.length; i++) {
+    let cardTextSubtitle = cardsTextSubtitle[i];
+    button.addEventListener('click', function () { 
+        cardTextSubtitle.innerHTML = arr[Math.floor(Math.random() * arr.length)];                   
+    });           
+}; 
+
 
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
     card.addEventListener('click', function () { 
-        card.firstElementChild.classList.toggle('card__back-side--rotate');;         
-        card.lastElementChild.classList.toggle('card__front-side--rotate');         
+        card.firstElementChild.classList.toggle('card__back-side--rotate');        
+        card.lastElementChild.classList.toggle('card__front-side--rotate');                
     });
+    
 };
 
 
